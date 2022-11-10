cdabf9d49c01a633f3242c5e3efc00fa145f37e1
Matt Jones <gitcode@magisa.org>
2022-11-09 16:07:06 -0900

    Update dependencies and package documentation.  Please enter the commit message for your changes. Lines starting

--- src/hashstore/__init__.py
+++ src/hashstore/__init__.py
@@ -1,1 +1,17 @@
+"""HashStore is a hash-based object store for data packages. It uses 
+cryptographic hash functions to name files consistently.
+HashStore creates a directory where objects and metadata are 
+stored using a hash value as the name.
+
+HashStore is mainly focused on storing DataONE data package contents
+on a shared file system for simple and fast access by data management
+processes that function across a cluster environment. Some properties:
+
+- Data objects are immutable and never change
+- Data objects are named using the SHA-256, base64-encoded hash of their contents (thus, a content-identifiers)
+- Metadata objects are stored with a pointer to the hash of the data object they describe
+- Metadata objects are named using the SHA-256, base64-encoded hash of their persistent identifier (PID)
+- An object's persistent identifier can be used to read both metadata and contents of the object
+"""
+
 from hashstore.hashstore import HashStore

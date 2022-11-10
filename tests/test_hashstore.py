b2d657e2df81b2a1ee2ff1c45c4a89d5b39a5650
Matt Jones <gitcode@magisa.org>
2022-11-09 18:43:24 -0900

    Mode HashStore to a test fixture.

--- tests/test_hashstore.py
+++ tests/test_hashstore.py
@@ -12,19 +12,21 @@ def pids():
     }
     return pids
 
+@pytest.fixture
+def store():
+    store = HashStore(store_path="/tmp/metacat")
+    return store
 
 def test_pids_length(pids):
     assert len(pids) == 3
 
 
-def test_init():
-    store = HashStore(store_path="/tmp/metacat")
+def test_init(store):
     value = store.version()
     assert value == importlib.metadata.version('hashstore')
 
 
-def test_add_files(pids):
-    store = HashStore(store_path="/tmp/metacat")
+def test_add_files(pids, store):
     test_dir = "tests/testdata/"
     for pid in pids.keys():
         path = test_dir + pid.replace("/", "_")
@@ -37,15 +39,13 @@ def test_add_files(pids):
     assert store.count() == 3
 
 
-def test_hash_string(pids):
-    store = HashStore(store_path="/tmp/metacat")
+def test_hash_string(pids, store):
     for pid in pids:
         hash_val = store.hash_string(pid)
         assert hash_val == pids[pid]
 
 
-def test_path(pids):
-    store = HashStore(store_path="/tmp/metacat")
+def test_path(pids, store):
     path = store.rel_path(pids["doi:10.18739/A2901ZH2M"])
     assert len(path) == 67
     assert path.startswith("0d/55/5e/d7")

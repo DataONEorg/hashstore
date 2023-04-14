c07e82b424bca2e7215fc126540f5755f2b49bc9
Dou Mok <douming.mok@gmail.com>
2023-04-14 09:54:28 -0700

    Update tests for clarity and accuracy

--- tests/test_hashstore.py
+++ tests/test_hashstore.py
@@ -11,7 +11,7 @@ import pytest
 def pids():
     pids = {
         "doi:10.18739/A2901ZH2M": {
-            "s_cid": "0d555ed77052d7e166017f779cbc193357c3a5006ee8b8457230bcf7abcef65e",
+            "ab_id": "0d555ed77052d7e166017f779cbc193357c3a5006ee8b8457230bcf7abcef65e",
             "md5": "db91c910a3202478c8def1071c54aae5",
             "sha1": "1fe86e3c8043afa4c70857ca983d740ad8501ccd",
             "sha224": "922b1e86f83d3ea3060fd0f7b2cf04476e8b3ddeaa3cf48c2c3cf502",
@@ -20,7 +20,7 @@ def pids():
             "sha512": "e9bcd6b91b102ef5803d1bd60c7a5d2dbec1a2baf5f62f7da60de07607ad6797d6a9b740d97a257fd2774f2c26503d455d8f2a03a128773477dfa96ab96a2e54",
         },
         "jtao.1700.1": {
-            "s_cid": "a8241925740d5dcd719596639e780e0a090c9d55a5d0372b0eaf55ed711d4edf",
+            "ab_id": "a8241925740d5dcd719596639e780e0a090c9d55a5d0372b0eaf55ed711d4edf",
             "md5": "f4ea2d07db950873462a064937197b0f",
             "sha1": "3d25436c4490b08a2646e283dada5c60e5c0539d",
             "sha224": "9b3a96f434f3c894359193a63437ef86fbd5a1a1a6cc37f1d5013ac1",
@@ -29,7 +29,7 @@ def pids():
             "sha512": "bf9e7f4d4e66bd082817d87659d1d57c2220c376cd032ed97cadd481cf40d78dd479cbed14d34d98bae8cebc603b40c633d088751f07155a94468aa59e2ad109",
         },
         "urn:uuid:1b35d0a5-b17a-423b-a2ed-de2b18dc367a": {
-            "s_cid": "7f5cc18f0b04e812a3b4c8f686ce34e6fec558804bf61e54b176742a7f6368d6",
+            "ab_id": "7f5cc18f0b04e812a3b4c8f686ce34e6fec558804bf61e54b176742a7f6368d6",
             "md5": "e1932fc75ca94de8b64f1d73dc898079",
             "sha1": "c6d2a69a3f5adaf478ba796c114f57b990cf7ad1",
             "sha224": "f86491d23d25dbaf7620542f056aba8a092a70be625502a6afd1fde0",
@@ -66,8 +66,9 @@ def test_store_files(pids, store):
         syspath = Path(test_dir) / filename
         sysmeta = syspath.read_bytes()
         hash_address = store.store_object(pid, path)
-        pid_hash = store.store_sysmeta(pid, sysmeta)
-        assert store.objects.exists(pid_hash)
+        ab_id = store.store_sysmeta(pid, sysmeta)
+        assert store.objects.exists(ab_id)
+        assert store.sysmeta.exists(ab_id)
     assert store.objects.count() == 3
     assert store.sysmeta.count() == 3
 
@@ -77,9 +78,8 @@ def test_store_address_length(pids, store):
     for pid in pids.keys():
         path = test_dir + pid.replace("/", "_")
         hash_address = store.store_object(pid, path)
-        pid_hash = hash_address.id
-        assert len(pid_hash) == 64
-        assert store.objects.exists(pid_hash)
+        ab_id = hash_address.id
+        assert len(ab_id) == 64
 
 
 def test_store_hex_digests(pids, store):
@@ -92,8 +92,6 @@ def test_store_hex_digests(pids, store):
         assert hash_address.hex_digests.get("sha256") == pids[pid]["sha256"]
         assert hash_address.hex_digests.get("sha384") == pids[pid]["sha384"]
         assert hash_address.hex_digests.get("sha512") == pids[pid]["sha512"]
-        pid_hash = store.objects._get_sha256_hex_digest(pid)
-        assert store.objects.exists(pid_hash)
 
 
 def test_store_input_stream(pids, store):
@@ -102,14 +100,9 @@ def test_store_input_stream(pids, store):
         path = test_dir + pid.replace("/", "_")
         input_stream = io.open(path, "rb")
         hash_address = store.store_object(pid, input_stream)
-        assert hash_address.hex_digests.get("md5") == pids[pid]["md5"]
-        assert hash_address.hex_digests.get("sha1") == pids[pid]["sha1"]
-        assert hash_address.hex_digests.get("sha256") == pids[pid]["sha256"]
-        assert hash_address.hex_digests.get("sha384") == pids[pid]["sha384"]
-        assert hash_address.hex_digests.get("sha512") == pids[pid]["sha512"]
         input_stream.close()
-        pid_hash = store.objects._get_sha256_hex_digest(pid)
-        assert store.objects.exists(pid_hash)
+        ab_id = store.objects._get_sha256_hex_digest(pid)
+        assert store.objects.exists(ab_id)
     return
 
 
@@ -128,10 +121,10 @@ def test_store_object_algorithm_args_hyphen(pids, store):
     path = test_dir + pid
     algorithm_with_hyphen_and_upper = "SHA-256"
     hash_address = store.store_object(pid, path, algorithm_with_hyphen_and_upper)
-    cid = hash_address.hex_digests.get("sha256")
-    assert cid == pids[pid]["sha256"]
-    pid_hash = store.objects._get_sha256_hex_digest(pid)
-    assert store.objects.exists(pid_hash)
+    sha256_cid = hash_address.hex_digests.get("sha256")
+    assert sha256_cid == pids[pid]["sha256"]
+    ab_id = store.objects._get_sha256_hex_digest(pid)
+    assert store.objects.exists(ab_id)
 
 
 def test_store_object_algorithm_args_other(store):
@@ -140,11 +133,11 @@ def test_store_object_algorithm_args_other(store):
     path = test_dir + pid
     algorithm_other = "sha3_256"
     hash_address = store.store_object(pid, path, algorithm_other)
-    additional_hex_digest = hash_address.hex_digests.get("sha3_256")
+    additional_sha3_256_hex_digest = hash_address.hex_digests.get("sha3_256")
     sha3_256_checksum = (
         "b748069cd0116ba59638e5f3500bbff79b41d6184bc242bd71f5cbbb8cf484cf"
     )
-    assert additional_hex_digest == sha3_256_checksum
+    assert additional_sha3_256_hex_digest == sha3_256_checksum
     pid_hash = store.objects._get_sha256_hex_digest(pid)
     assert store.objects.exists(pid_hash)
 
@@ -155,13 +148,13 @@ def test_store_object_algorithm_args_other_hyphen(store):
     path = test_dir + pid
     algorithm_other = "sha3-256"
     hash_address = store.store_object(pid, path, algorithm_other)
-    cid = hash_address.hex_digests.get("sha3_256")
+    additional_sha3_256_hex_digest = hash_address.hex_digests.get("sha3_256")
     sha3_256_checksum = (
         "b748069cd0116ba59638e5f3500bbff79b41d6184bc242bd71f5cbbb8cf484cf"
     )
-    assert cid == sha3_256_checksum
-    pid_hash = store.objects._get_sha256_hex_digest(pid)
-    assert store.objects.exists(pid_hash)
+    assert additional_sha3_256_hex_digest == sha3_256_checksum
+    ab_id = store.objects._get_sha256_hex_digest(pid)
+    assert store.objects.exists(ab_id)
 
 
 def test_store_object_algorithm_args_incorrect_checksum(store):
@@ -175,25 +168,26 @@ def test_store_object_algorithm_args_incorrect_checksum(store):
     with pytest.raises(ValueError):
         store.store_object(path, algorithm_other, checksum_incorrect)
     assert store.objects.count() == 0
-    pid_hash = store.objects._get_sha256_hex_digest(pid)
-    assert store.objects.exists(pid_hash) is False
+    ab_id = store.objects._get_sha256_hex_digest(pid)
+    assert store.objects.exists(ab_id) is False
 
 
 def test_store_duplicate_objects(store):
     test_dir = "tests/testdata/"
     pid = "jtao.1700.1"
     path = test_dir + pid
-    hash_address = store.store_object(pid, path)
-    hash_address_duplicate = store.store_object(pid, path)
-    assert hash_address_duplicate.is_duplicate is True
+    # Store first blob
+    hash_address_one = store.store_object(pid, path)
+    # Store second blob
+    hash_address_two = store.store_object(pid, path)
+    assert hash_address_two.is_duplicate is True
     assert store.objects.count() == 1
-    pid_hash = store.objects._get_sha256_hex_digest(pid)
-    assert store.objects.exists(pid_hash)
+    ab_id = store.objects._get_sha256_hex_digest(pid)
+    assert store.objects.exists(ab_id)
 
 
 def test_store_duplicate_object_threads(store):
     # FileExistsError can potentially be raised as a warning (expected)
-    # File count must be 1
     test_dir = "tests/testdata/"
     pid = "jtao.1700.1"
     path = test_dir + pid
@@ -206,12 +200,13 @@ def test_store_duplicate_object_threads(store):
     thread1.join()
     thread2.join()
     thread3.join()
+    # File count must be 1
     assert store.objects.count() == 1
-    pid_hash = store.objects._get_sha256_hex_digest(pid)
-    assert store.objects.exists(pid_hash)
+    ab_id = store.objects._get_sha256_hex_digest(pid)
+    assert store.objects.exists(ab_id)
 
 
-def test_store_sysmeta_s_cid(pids, store):
+def test_store_sysmeta_ab_id(pids, store):
     test_dir = "tests/testdata/"
     for pid in pids.keys():
         path = test_dir + pid.replace("/", "_")
@@ -219,8 +214,8 @@ def test_store_sysmeta_s_cid(pids, store):
         syspath = Path(test_dir) / filename
         sysmeta = syspath.read_bytes()
         hash_address = store.store_object(pid, path)
-        s_cid = store.store_sysmeta(pid, sysmeta)
-        assert s_cid == pids[pid]["s_cid"]
+        ab_id = store.store_sysmeta(pid, sysmeta)
+        assert ab_id == pids[pid]["ab_id"]
 
 
 def test_store_sysmeta_cid(pids, store):
@@ -233,8 +228,8 @@ def test_store_sysmeta_cid(pids, store):
         hash_address = store.store_object(pid, path)
         store.store_sysmeta(pid, sysmeta)
         s_content = store._get_sysmeta(pid)
-        cid_get = s_content[0][:64]
-        assert cid_get == pids[pid]["s_cid"]
+        ab_id_get = s_content[0][:64]
+        assert ab_id_get == pids[pid]["ab_id"]
 
 
 def test_store_sysmeta_update(store):
@@ -246,13 +241,13 @@ def test_store_sysmeta_update(store):
     syspath = Path(test_dir) / filename
     sysmeta = syspath.read_bytes()
     hash_address = store.store_object(pid, path)
-    s_cid = store.store_sysmeta(pid, sysmeta)
-    pid_new = s_cid[::-1]
+    ab_id = store.store_sysmeta(pid, sysmeta)
+    pid_new = ab_id[::-1]
     store.store_object(pid_new, path)
-    s_cid = store.store_sysmeta(pid_new, sysmeta)
+    ab_id_new = store.store_sysmeta(pid_new, sysmeta)
     s_content = store._get_sysmeta(pid_new)
-    cid_get = s_content[0][:64]
-    assert s_cid == cid_get
+    ab_id_get = s_content[0][:64]
+    assert ab_id_new == ab_id_get
 
 
 def test_store_sysmeta_thread_lock(store):
@@ -277,9 +272,9 @@ def test_store_sysmeta_thread_lock(store):
     thread2.join()
     thread3.join()
     thread4.join()
-    pid_hash = store.objects._get_sha256_hex_digest(pid)
-    s_cid = store._get_sysmeta(pid)[0][:64]
-    assert s_cid == pid_hash
+    ab_id = store.objects._get_sha256_hex_digest(pid)
+    ab_id_get = store._get_sysmeta(pid)[0][:64]
+    assert ab_id_get == ab_id
     assert store.objects.count() == 1
     assert store.sysmeta.count() == 1
 
@@ -314,8 +309,7 @@ def test_retrieve_sysmeta(store):
     syspath = Path(test_dir) / filename
     sysmeta = syspath.read_bytes()
     hash_address = store.store_object(pid, path)
-    cid = hash_address.hex_digests.get("sha256")
-    s_cid = store.store_sysmeta(pid, sysmeta)
+    ab_id = store.store_sysmeta(pid, sysmeta)
     sysmeta_ret = store.retrieve_sysmeta(pid)
     assert sysmeta.decode("utf-8") == sysmeta_ret
 
@@ -335,8 +329,7 @@ def test_delete(pids, store):
         syspath = Path(test_dir) / filename
         sysmeta = syspath.read_bytes()
         hash_address = store.store_object(pid, path)
-        cid = hash_address.hex_digests.get("sha256")
-        s_cid = store.store_sysmeta(pid, sysmeta)
+        ab_id = store.store_sysmeta(pid, sysmeta)
         store.delete_object(pid)
         store.delete_sysmeta(pid)
     assert store.objects.count() == 0
@@ -351,7 +344,7 @@ def test_get_hex_digest(store):
     syspath = Path(test_dir) / filename
     sysmeta = syspath.read_bytes()
     hash_address = store.store_object(pid, path)
-    s_cid = store.store_sysmeta(pid, sysmeta)
+    ab_id = store.store_sysmeta(pid, sysmeta)
     sha3_256_hex_digest = (
         "b748069cd0116ba59638e5f3500bbff79b41d6184bc242bd71f5cbbb8cf484cf"
     )
@@ -374,7 +367,7 @@ def test_get_hex_digest_pid_unsupported_algorithm(store):
     filename = pid + ".xml"
     syspath = Path(test_dir) / filename
     syspath.read_bytes()
-    store.store_object(pid, path)
+    hash_address = store.store_object(pid, path)
     algorithm = "sm3"
     with pytest.raises(ValueError):
         store.get_hex_digest(pid, algorithm)
@@ -383,11 +376,11 @@ def test_get_hex_digest_pid_unsupported_algorithm(store):
 def test_get_sha256_hex_digest(pids, store):
     for pid in pids:
         hash_val = store.objects._get_sha256_hex_digest(pid)
-        assert hash_val == pids[pid]["s_cid"]
+        assert hash_val == pids[pid]["ab_id"]
 
 
 def test_rel_path(pids, store):
-    path = store._rel_path(pids["doi:10.18739/A2901ZH2M"]["s_cid"])
+    path = store._rel_path(pids["doi:10.18739/A2901ZH2M"]["ab_id"])
     assert len(path) == 67
     assert path.startswith("0d/55/5e/d7")
     assert path.endswith("7052d7e166017f779cbc193357c3a5006ee8b8457230bcf7abcef65e")
@@ -409,33 +402,33 @@ def test_computehash(pids, store):
     test_dir = "tests/testdata/"
     for pid in pids.keys():
         path = test_dir + pid.replace("/", "_")
-        cid_stream = io.open(path, "rb")
-        cid_hash = store.objects.computehash(cid_stream, "sha256")
-        cid_stream.close()
-        assert pids[pid]["sha256"] == cid_hash
+        obj_stream = io.open(path, "rb")
+        obj_sha256_hash = store.objects.computehash(obj_stream, "sha256")
+        obj_stream.close()
+        assert pids[pid]["sha256"] == obj_sha256_hash
 
 
 def test_put(pids, store):
     test_dir = "tests/testdata/"
     for pid in pids.keys():
         path = test_dir + pid.replace("/", "_")
-        pid_hashaddress = store.objects.put(pid, path)
-        pid_id = pid_hashaddress.id
-        pid_relpath = pid_hashaddress.relpath
-        pid_abspath = pid_hashaddress.abspath
-        pid_is_duplicate = pid_hashaddress.is_duplicate
-        pid_hex_digests = pid_hashaddress.hex_digests
-        assert pid_id == pids[pid]["s_cid"]
-        shard_id_path = "/".join(store.objects.shard(pid_id))
-        assert pid_relpath == shard_id_path
-        id_abs_path = store.objects.realpath(pid_id)
-        assert pid_abspath == id_abs_path
-        assert pid_is_duplicate is False
-        assert pid_hex_digests.get("md5") == pids[pid]["md5"]
-        assert pid_hex_digests.get("sha1") == pids[pid]["sha1"]
-        assert pid_hex_digests.get("sha256") == pids[pid]["sha256"]
-        assert pid_hex_digests.get("sha384") == pids[pid]["sha384"]
-        assert pid_hex_digests.get("sha512") == pids[pid]["sha512"]
+        hashaddress = store.objects.put(pid, path)
+        hashaddress_id = hashaddress.id
+        hashaddress_relpath = hashaddress.relpath
+        hashaddress_abspath = hashaddress.abspath
+        hashaddress_is_duplicate = hashaddress.is_duplicate
+        hashaddress_hex_digests = hashaddress.hex_digests
+        assert hashaddress_id == pids[pid]["ab_id"]
+        shard_id_path = "/".join(store.objects.shard(hashaddress_id))
+        assert hashaddress_relpath == shard_id_path
+        id_abs_path = store.objects.realpath(hashaddress_id)
+        assert hashaddress_abspath == id_abs_path
+        assert hashaddress_is_duplicate is False
+        assert hashaddress_hex_digests.get("md5") == pids[pid]["md5"]
+        assert hashaddress_hex_digests.get("sha1") == pids[pid]["sha1"]
+        assert hashaddress_hex_digests.get("sha256") == pids[pid]["sha256"]
+        assert hashaddress_hex_digests.get("sha384") == pids[pid]["sha384"]
+        assert hashaddress_hex_digests.get("sha512") == pids[pid]["sha512"]
 
 
 def test_put_additional_algorithm(pids, store):
@@ -443,10 +436,10 @@ def test_put_additional_algorithm(pids, store):
     for pid in pids.keys():
         algo = "sha224"
         path = test_dir + pid.replace("/", "_")
-        pid_hashaddress = store.objects.put(pid, path, algorithm=algo)
-        pid_hex_digests = pid_hashaddress.hex_digests
-        pid_sha224 = pid_hex_digests.get(algo)
-        assert pid_sha224 == pids[pid][algo]
+        hash_address = store.objects.put(pid, path, algorithm=algo)
+        hex_digests = hash_address.hex_digests
+        sha224_hash = hex_digests.get(algo)
+        assert sha224_hash == pids[pid][algo]
 
 
 def test_put_with_correct_checksums(pids, store):
@@ -482,6 +475,8 @@ def test_move_and_get_checksums(pids, store):
             is_duplicate,
         ) = store.objects._move_and_get_checksums(pid, input_stream)
         input_stream.close()
+        ab_id = store.objects._get_sha256_hex_digest(pid)
+        assert id == ab_id
         assert hex_digests.get("md5") == pids[pid]["md5"]
         assert hex_digests.get("sha1") == pids[pid]["sha1"]
         assert hex_digests.get("sha256") == pids[pid]["sha256"]

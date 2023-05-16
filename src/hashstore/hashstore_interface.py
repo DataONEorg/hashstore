7d4e2b6f0305b2db3eece759f4e5333aa3b3edd6
Dou Mok <douming.mok@gmail.com>
2023-05-16 13:51:17 -0700

    Update 'hashstore_interface' signature values to resolve compiler warnings and update method doc strings

--- src/hashstore/hashstore_interface.py
+++ src/hashstore/hashstore_interface.py
@@ -8,7 +8,14 @@ class HashStoreInterface(ABC):
     value to address files."""
 
     @abstractmethod
-    def store_object(self):
+    def store_object(
+        self,
+        pid,
+        data,
+        additional_algorithm,
+        checksum,
+        checksum_algorithm,
+    ):
         """The `store_object` method is responsible for the atomic storage of objects to
         disk using a given InputStream and a persistent identifier (pid). Upon
         successful storage, the method returns a HashAddress object containing
@@ -23,21 +30,33 @@ class HashStoreInterface(ABC):
         delimited by '/' and concatenated to produce the final permanent address
         and is stored in the `/[...storeDirectory]/objects/` directory.
 
-        By default, the hex digest map includes the following hash algorithms: MD5,
-        SHA-1, SHA-256, SHA-384 and SHA-512, which are the most commonly used
-        algorithms in dataset submissions to DataONE and the Arctic Data Center. If
-        an additional algorithm is provided, the `store_object` method checks if it is
-        supported and adds it to the map along with its corresponding hex digest. An
-        algorithm is considered "supported" if it is recognized as a valid hash
-        algorithm in the `hashlib` libary.
+        By default, the hex digest map includes the following hash algorithms:
+        Default algorithms and hex digests to return: md5, sha1, sha256, sha384, sha512,
+        which are the most commonly used algorithms in dataset submissions to DataONE
+        and the Arctic Data Center. If an additional algorithm is provided, the
+        `store_object` method checks if it is supported and adds it to the map along
+        with its corresponding hex digest. An algorithm is considered "supported" if it
+        is recognized as a valid hash algorithm in the `hashlib` libary.
 
         Similarly, if a checksum and a checksumAlgorithm value are provided,
         `store_object` validates the object to ensure it matches what is provided
-        before moving the file to its permanent address."""
+        before moving the file to its permanent address.
+
+        Args:
+            pid (string): authority-based identifier
+            data (mixed): string or path to object
+            additional_algorithm (string): additional hex digest to include
+            checksum (string): checksum to validate against
+            checksum_algorithm (string): algorithm of supplied checksum
+
+        Returns:
+            address (HashAddress): object that contains the permanent address, relative
+            file path, absolute file path, duplicate file boolean and hex digest dictionary
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def store_sysmeta(self):
+    def store_sysmeta(self, pid, sysmeta):
         """The `store_sysmeta` method is responsible for adding and/or updating metadata
         (`sysmeta`) to disk using a given InputStream and a persistent identifier
         (pid). The metadata object consists of a header and body portion. The header
@@ -48,37 +67,81 @@ class HashStoreInterface(ABC):
         represents the file's permanent address, and similarly to 'store_object', this
         permanent address is determined by calculating the SHA-256 hex digest of the
         provided pid. Finally, sysmeta are stored in parallel to objects in the
-        `/[...storeDirectory]/sysmeta/` directory."""
+        `/[...storeDirectory]/sysmeta/` directory.
+
+        Args:
+            pid (string): authority-based identifier
+            sysmeta (mixed): string or path to sysmeta document
+
+        Returns:
+            sysmeta_cid (string): address of the sysmeta document
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def retrieve_object(self):
+    def retrieve_object(self, pid):
         """The `retrieve_object` method retrieves an object from disk using a given
         persistent identifier (pid). If the object exists (determined by calculating
         the object's permanent address using the SHA-256 hash of the given pid), the
-        method will open and return a buffered object stream ready to read from."""
+        method will open and return a buffered object stream ready to read from.
+
+        Args:
+            pid (string): authority-based identifier
+
+        Returns:
+            obj_stream (io.BufferedReader): a buffered stream of an ab_id object
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def retrieve_sysmeta(self):
+    def retrieve_sysmeta(self, pid):
         """The 'retrieve_sysmeta' method retrieves the metadata content from disk and
-        returns it in the form of a String using a given persistent identifier."""
+        returns it in the form of a String using a given persistent identifier.
+
+        Args:
+            pid (string): authority-based identifier
+
+        Returns:
+            sysmeta (string): sysmeta content
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def delete_object(self):
+    def delete_object(self, pid):
         """The 'delete_object' method deletes an object permanently from disk using a
-        given persistent identifier."""
+        given persistent identifier.
+
+        Args:
+            pid (string): authority-based identifier
+
+        Returns:
+            boolean: True upon successful deletion
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def delete_sysmeta(self):
+    def delete_sysmeta(self, pid):
         """The 'delete_sysmeta' method deletes an metadata document (sysmeta) permanently
-        from disk using a given persistent identifier."""
+        from disk using a given persistent identifier.
+
+        Args:
+            pid (string): authority-based identifier
+
+        Returns:
+            boolean: True upon successful deletion
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def get_hex_digest(self):
+    def get_hex_digest(self, pid, algorithm):
         """The 'get_hex_digest' method calculates the hex digest of an object that exists
-        in HashStore using a given persistent identifier and hash algorithm."""
+        in HashStore using a given persistent identifier and hash algorithm.
+
+        Args:
+            pid (string): authority-based identifier
+            algorithm (string): algorithm of hex digest to generate
+
+        Returns:
+            hex_digest (string): hex digest of the object
+        """
         raise NotImplementedError()

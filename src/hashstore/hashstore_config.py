ac3d0b76f4cb88cb44e4ea511ad9983652d8e7f0
Dou Mok <douming.mok@gmail.com>
2023-05-19 10:29:54 -0700

    Add comments & additional context in 'hashstore_config'

--- src/hashstore/hashstore_config.py
+++ src/hashstore/hashstore_config.py
@@ -1,6 +1,10 @@
 """Default configuration variables for HashStore"""
-DIR_DEPTH = 3
-DIR_WIDTH = 2
+# Default path for `FileHashStore` if no path is provided
+STORE_PATH = "/var/filehashstore/"
+# Desired amount of directories when sharding an object to form the permanent address
+DIR_DEPTH = 3 # DO NOT CHANGE UNLESS SETTING UP NEW HASHSTORE
+# Width of directories created when sharding an object to form the permanent address
+DIR_WIDTH = 2 # DO NOT CHANGE UNLESS SETTING UP NEW HASHSTORE
 SYSMETA_NS = "http://ns.dataone.org/service/types/v2.0"
 # Algorithm values supported by python hashlib 3.9.0+
 DEFAULT_ALGO_LIST = ["sha1", "sha256", "sha384", "sha512", "md5"]

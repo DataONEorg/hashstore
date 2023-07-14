d83d31010cac99a423cf078dfd4db39bd2e21b69
Dou Mok <douming.mok@gmail.com>
2023-07-14 12:06:10 -0700

    Add initial positional and optional arguments

--- src/hashstore/client.py
+++ src/hashstore/client.py
@@ -8,10 +8,23 @@ if __name__ == "__main__":
         + " into a hashstore and perform operations to store, retrieve,"
         + " and delete the objects."
     )
-    EPILOG = "By Dou Mok"
+    EPILOG = "Created for DataONE (NCEAS)"
     parser = ArgumentParser(
         prog=PROGRAM_NAME,
         description=DESCRIPTION,
         epilog=EPILOG,
     )
-    parser.parse_args()
+
+    # Positional Arguments
+    # Path of the HashStore to create and/or store/delete objects to/from
+    parser.add_argument("store_path", help="Path of the HashStore")
+
+    # Optional Arguments
+    parser.add_argument("-chs", dest="action", help="Create a HashStore")
+    parser.add_argument("-sobj", dest="action", help="Store an object to the HashStore")
+    parser.add_argument(
+        "-dobj", dest="action", help="Delete an object to the HashStore"
+    )
+    parser.add_argument("-pid", dest="pid", help="Object Identifier")
+
+    parser.parse_args(["--help"])
